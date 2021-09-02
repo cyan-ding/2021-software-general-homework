2c6b8c31b7d8e1215ea5461ea41ec8d8fc61a0e0
Cyan <leiwang@Lei-MBP2016.local>
2021-09-01 19:53:39 -0700

    initial commit

--- lesson_1.py
+++ lesson_1.py
@@ -6,4 +6,18 @@
 	# Rookie Year (named rookie_year)
 	# Is Active (named is_active)
 # Be sure to store each variable as the correct type!
-print("lol :D")
+team_number = str(input("What is the team number? "))
+team_name = str(input("What is the team name? "))
+location = input("Where is the team located? ")
+rookie_year = int(input("What year was the team started? "))
+is_active = bool(input("Is the team still active? "))
+print(team_number)
+print(team_name)
+print(location)
+print(rookie_year)
+print(is_active)
+print("Team", team_name, "aka as", team_number,"is based in",location, "and was started in the year",rookie_year)
+if is_active == True:
+    print("Still Active: ", True)
+else:
+    print("Still Active: ", False)
